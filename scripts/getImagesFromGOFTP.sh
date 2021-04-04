3b6b2e4fb9f1ffe4c8485621eeab22dc09359993
Nicolai R. Tellefsen <niccofyren@gmail.com>
2021-04-04 16:36:38 +0200

    FTP now has https

--- scripts/getImagesFromGOFTP.sh
+++ scripts/getImagesFromGOFTP.sh
@@ -6,7 +6,7 @@ then
   echo "./ftp-files already exists skipping ftp fetching (this data should generally be pretty static)"
 else
   export LC_ALL=C.UTF-8
-  lftp -c "open http://ftp.gathering.org/TG/ && find -l && exit" >> ftp-files
+  lftp -c "open https://ftp.gathering.org/TG/ && find -l && exit" >> ftp-files
 fi
 
 echo "Converting relevant images paths to ftp urls, writing results to ./ftp-image-urls"
@@ -26,7 +26,7 @@ cat ftp-files | \
   # Only allow certain filetypes
   grep '\(png\|jpg\|jpeg\)$' | \
   # Append the full url, and output
-  sed -e 's/\.\//http\:\/\/ftp\.gathering\.org\/TG\//' > ftp-image-urls
+  sed -e 's/\.\//https\:\/\/ftp\.gathering\.org\/TG\//' > ftp-image-urls
 
 
 echo "Running JS script to convert ftp urls into proper image objects, writing rsults to ./images.json"
